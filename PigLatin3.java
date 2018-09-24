3b2ee9a65fd16dafc6f274a0d986d31cd87d5273
kscott82 <43094692+kscott82@users.noreply.github.com>
2018-09-23 19:10:23 -0700

    Piglatin
    imports from .txt file

---
+++ PigLatin3.java
@@ -0,0 +1,30 @@
+/* Progaram: GuessingGame
+ * GuessingGame.java
+ * Summary: User Makes Guess of Interger between 1 and 10000
+ * Author: Kriztofer Scott
+* September 22 , 2018 
+ */
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.Formatter;
+
+public class PigLatin3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+         File temp = new File ("Pigs.txt");
+         Scanner file = new Scanner (temp); 
+         
+         System.out.println (file.nextLine());
+         System.out.println (file.nextLine());
+         System.out.println (file.nextLine());
+         System.out.println (file.nextLine());
+         System.out.println (file.nextLine());
+    
+
+    }
+    
+}
